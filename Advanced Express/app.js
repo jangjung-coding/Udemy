4f62f00295a475e65465aa47c8617f1ca0a2f1a7
jangjung-coding <jangisalive1650@gmail.com>
2023-07-26 00:14:55 +0900

    7/26

---
+++ Advanced Express/app.js
@@ -0,0 +1,56 @@
+const fs = require("fs"); // import fs module
+const path = require("path"); // import path module
+
+const express = require("express"); // import express
+
+const app = express(); // initialize express
+
+app.use(express.static('public')); // use middleware
+app.use(express.urlencoded({ extended: true })); // use middleware
+
+// app.get("/", function (req, res) {
+//   res.send("<h1>Hello!</h1>");
+// });
+
+app.get("/", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+});
+
+
+app.get("/restaurants", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.post('/recommend', function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm');
+});
+
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.listen(3000); // listen for requests on port 3000
