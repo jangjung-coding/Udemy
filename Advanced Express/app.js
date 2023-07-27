d418bc1fa557266adfef1101307138e2c9973a84
jangjung-coding <jangisalive1650@gmail.com>
2023-07-27 14:18:03 +0900

    7/27

--- Advanced Express/app.js
+++ Advanced Express/app.js
@@ -5,7 +5,10 @@ const express = require("express"); // import express
 
 const app = express(); // initialize express
 
-app.use(express.static('public')); // use middleware
+app.set("views", path.join(__dirname, "views")); // set views directory (default: views directory in root folder of project directory)
+app.set("view engine", "ejs"); // set view engine to ejs (embedded javascript templating)
+
+app.use(express.static("public")); // use middleware
 app.use(express.urlencoded({ extended: true })); // use middleware
 
 // app.get("/", function (req, res) {
@@ -13,24 +16,34 @@ app.use(express.urlencoded({ extended: true })); // use middleware
 // });
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "index.html");
+  // res.sendFile(htmlFilePath);
+  res.render("index");
 });
 
-
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  // res.sendFile(htmlFilePath);
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
 app.get("/recommend", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  // res.sendFile(htmlFilePath);
+  res.render("recommend");
 });
 
-app.post('/recommend', function (req, res) {
+app.post("/recommend", function (req, res) {
   const restaurant = req.body;
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+  const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
@@ -39,18 +52,19 @@ app.post('/recommend', function (req, res) {
 
   fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
 
-  res.redirect('/confirm');
+  res.redirect("/confirm");
 });
 
-
 app.get("/confirm", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  // res.sendFile(htmlFilePath);
+  res.render("confirm");
 });
 
 app.get("/about", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "about.html");
+  // res.sendFile(htmlFilePath);
+  res.render("about");
 });
 
 app.listen(3000); // listen for requests on port 3000
